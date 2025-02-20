73a34fd93b3a36a2813d43defcd2a77aec4913e1
AhmedAbdi-Dev <fimalissm@gmail.com>
2025-02-20 23:24:34 +0100

    Basic Animations

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -6,74 +8,52 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: "pink" })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-const group = new THREE.Group()
-group.position.y = 1
-group.scale.y= 2
-scene.add(group) 
-
-const cubb1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-
-group.add(cubb1)
-
-
-const cube2= new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: "blue"})
-)
-cube2.position.set(2,0,0)
-group.add(cube2)
-
-
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: "green"})
-)
-cube3.position.set(-2,0,0)
-group.add(cube3)
-
-
-
-//Axes helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// camera.position.x = 1
-// camera.position.y = 1
 camera.position.z = 3
-
-
 scene.add(camera)
 
-
-
-
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+
+
+//Clock
+// const clock = new THREE.Clock()
+
+gsap.to(mesh.position, {x: 2, duration: 1, delay: 1})
+gsap.to(mesh.position, {x: 0, duration: 1, delay: 2})
+
+///Animation
+const tick = () => {
+
+//clock
+// const elapsedTime = clock.getElapsedTime()
+
+//update objects
+// mesh.rotation.x -=0.01
+// mesh.position.y = Math.sin(elapsedTime)  
+// mesh.position.x = Math.cos(elapsedTime)  
+
+
+//render
 renderer.render(scene, camera)
+window.requestAnimationFrame(tick)
+
+}
+
+tick()
