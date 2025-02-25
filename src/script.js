8a1debc6b0f5874f9cbc73e54b0388effe0c4937
AhmedAbdi-Dev <fimalissm@gmail.com>
2025-02-25 10:38:41 +0100

    Fullscreen and resizing

--- src/script.js
+++ src/script.js
@@ -1,20 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-//In this code I can drag and move around the cube with dampening
-
-//Cursor
-const cursor ={
-    x:0,
-    y:0
-}
-
-
-window.addEventListener('mousemove', (event) => 
-    {
-        cursor.x = event.clientX / sizes.width - 0.5
-        cursor.y = -(event.clientY / sizes.height - 0.5)
-})
+//THIS CODE INCLUDES FULL SCREEN AND RESIZING FUNCTIONALITY 
 
 /**
  * Base
@@ -22,72 +9,105 @@ window.addEventListener('mousemove', (event) =>
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
-
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Camera
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+
+// //Resize
+window.addEventListener('resize', () => {
+    //update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+})
+
+/// FULL SCREEN CODE THAT WORKS IN ALL BROWSERS
+window.addEventListener('dblclick', () => 
+{
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen()
+
+        }
+
+    }
+    else {
+        if (document.exitFullscreen) 
+            {
+            document.exitFullscreen()
+
+        }
+        else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+
+        }
+    }
+})
+
+
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// const aspectRatio= sizes.width / sizes.height
-// console.log 
-// const camera = new THREE.OrthographicCamera
-// (-1* aspectRatio,
-// 1 * aspectRatio, 
-//     1 , -1, 0.1, 100)
-// camera.position.x = 2
-// camera.position.y = 2
 camera.position.z = 3
-camera.lookAt(mesh.position)
 scene.add(camera)
 
-//controls
+// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// controls.target.y = 1
-controls.update()
-
-
-
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    // mesh.rotation.y = elapsedTime;
-
-
-    // update camera your updating the position of camera to mimic the cursor
-
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    // camera.position.y = cursor.y * 5
-    // camera.lookAt(mesh.position)
-
-    //update camera controls
+    // Update controls
     controls.update()
-
 
     // Render
     renderer.render(scene, camera)
