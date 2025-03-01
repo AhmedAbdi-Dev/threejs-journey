d98ee7fe7b7db0f76fa99dab371631334c35a016
AhmedAbdi-Dev <fimalissm@gmail.com>
2025-03-01 23:43:35 +0100

    Debug-ui (lil-gui)

--- src/script.js
+++ src/script.js
@@ -1,7 +1,29 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import GUI from 'lil-gui'
 
-//THIS CODE INCLUDES GEOMETRY/ POSITIONARRAY / POSITION ATTRIBUTE 
+//LIL GUI
+
+const gui = new GUI({
+    width: 300,
+    title: 'Nice Debug UI',
+    closeFolders: false,
+
+})
+// gui.close()
+// gui.hide()
+
+//TOGGLE GUI 
+window.addEventListener('keydown', (event) => {
+    if  (event.key === 'h') {
+        gui.show(gui._hidden)
+}
+})
+
+
+const debugObject ={
+}
 
 /**
  * Base
@@ -12,43 +34,75 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// GEOMETRY ATTRBIUTES and VERTICES
-
-const geometry = new THREE.BufferGeometry()
-
-const count = 500
-
-const positionsArray = new Float32Array(count * 3 *3)
-
-for (let i = 0; i < count * 3 * 3; i++) {
-    positionsArray[i] = (Math.random()-0.5) * 4 
-
-}
-
-const postionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
-
-geometry.setAttribute('position', postionsAttribute)
-
-
-
-const positionAttribute = new THREE.BufferAttribute(positionsArray, 3)
-
-
-geometry.setAttribute('position', positionAttribute)
-
-
-
-
 /**
  * Object
  */
+debugObject.color ='#27b953'
 
-const material = new THREE.MeshBasicMaterial
-({ color: 0xff0000, 
-    wireframe:true
-})
+
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true  })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+//FOLDERS
+const cubeTweaks = gui.addFolder('Cube')
+// cubeTweaks.close()
+
+cubeTweaks
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+
+
+cubeTweaks
+    .add(mesh.position, 'x')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+
+
+cubeTweaks
+    .add(mesh, 'visible')
+
+
+cubeTweaks 
+    .add(material, "wireframe")
+
+cubeTweaks 
+    .addColor(debugObject, "color")
+    .onChange(() => 
+    {
+        material.color.set(debugObject.color)
+
+    }
+)
+
+debugObject.spin = () => { 
+    gsap.to(mesh.rotation, {y: mesh.rotation.y + Math.PI * 2 })
+
+}
+
+cubeTweaks
+    .add(debugObject, "spin")
+
+debugObject.subdivisions = 2
+
+cubeTweaks
+    .add(debugObject, 'subdivisions')
+    .min(1)
+    .max(20)
+    .step(1)
+    .onFinishChange(
+    () => {
+        mesh.geometry.dispose()
+        mesh.geometry= new THREE.BoxGeometry
+        (1, 1, 1, 
+            debugObject.subdivisions, debugObject.subdivisions, debugObject.subdivisions)
+    })
+
 
 /**
  * Sizes
@@ -58,59 +112,29 @@ const sizes = {
     height: window.innerHeight
 }
 
-
-// //Resize
-window.addEventListener('resize', () => {
-    //update sizes
+window.addEventListener('resize', () =>
+{
+    // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    //update camera
+    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    //update renderer
+    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
 })
-
-/// FULL SCREEN CODE THAT WORKS IN ALL BROWSERS
-window.addEventListener('dblclick', () => 
-{
-    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-
-    if (!fullscreenElement) {
-        if (canvas.requestFullscreen) {
-            canvas.requestFullscreen()
-        }
-        else if(canvas.webkitRequestFullscreen) {
-            canvas.webkitRequestFullscreen()
-
-        }
-
-    }
-    else {
-        if (document.exitFullscreen) 
-            {
-            document.exitFullscreen()
-
-        }
-        else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen()
-
-        }
-    }
-})
-
 
 /**
  * Camera
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -125,7 +149,6 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
 
 /**
  * Animate
